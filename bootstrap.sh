0a589c03e2f6c9b684444a41f9ce36164ad38c3b
Ivan Vashchaev <vivkin@gmail.com>
2015-02-16 01:46:07 +0300

    update plugins and quit

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,3 +13,5 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
   mkdir -p ~/.vim/autoload
   curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+vim -c PlugUpdate -c qa
