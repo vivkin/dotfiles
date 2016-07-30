51d20872d4f977cc09252a2cc04a072478f38132
Ivan Vashchaev <vivkin@gmail.com>
2016-07-30 10:27:23 +0300

    не ту часть у файла отрезал :)

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ if [ -z "$XDG_CONFIG_HOME" ]; then
 fi
 
 mkdir -pv $XDG_DATA_HOME/nvim/{backup,swap,undo,view} && echo "Bootstrapped vim dirs"
-mkdir -pv "${HISTFILE##*/}" "${LESSHISTFILE##*/}" && echo "Bootstrapped history dirs"
+mkdir -pv "${HISTFILE%/*}" "${LESSHISTFILE%/*}" && echo "Bootstrapped history dirs"
 ln -fhsv {$XDG_CONFIG_HOME/,~/.}profile && echo "Bootstrapped profile"
 [[ $(uname) == "Darwin" ]] && ln -fhsv {$XDG_CONFIG_HOME/,~/Library/LaunchAgents/}org.freedesktop.xdg.plist && echo "Bootstrapped launchd"
