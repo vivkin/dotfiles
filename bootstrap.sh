19dbeaa275317aee2ec8135238077317e9936861
Ivan Vashchaev <vivkin@gmail.com>
2016-07-30 11:04:27 +0300

    replace ln -h to ln -n

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,5 @@ fi
 
 mkdir -pv $XDG_DATA_HOME/nvim/{backup,swap,undo,view} && echo "Bootstrapped vim dirs"
 mkdir -pv "${HISTFILE%/*}" "${LESSHISTFILE%/*}" && echo "Bootstrapped history dirs"
-ln -fhsv {$XDG_CONFIG_HOME/,~/.}profile && echo "Bootstrapped profile"
-[[ $(uname) == "Darwin" ]] && ln -fhsv {$XDG_CONFIG_HOME/,~/Library/LaunchAgents/}org.freedesktop.xdg.plist && echo "Bootstrapped launchd"
+ln -fnsv {$XDG_CONFIG_HOME/,~/.}profile && echo "Bootstrapped profile"
+[[ $(uname) == "Darwin" ]] && ln -fnsv {$XDG_CONFIG_HOME/,~/Library/LaunchAgents/}org.freedesktop.xdg.plist && echo "Bootstrapped launchd"
