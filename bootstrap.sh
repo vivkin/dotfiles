d88b4e549ab49255849f2517644b060fb15d18b7
Ivan Vashchaev <vivkin@gmail.com>
2016-07-30 10:06:02 +0300

    update bootstraper

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-mkdir -p ~/dotfiles/backup
-for file in bashrc clang-format editrc inputrc vimrc gitconfig; do
-  if [ ~/.$file -ot ~/dotfiles/$file ]; then
-      cp ~/.$file ~/dotfiles/backup/$file-$(date +%Y%m%d-%H%M%S)
-  fi
-  ln -si ~/dotfiles/$file ~/.$file
-done
+if [ -z "$XDG_CONFIG_HOME" ]; then
+	echo "Please source profile first"
+	exit
+fi
+
+mkdir -pv $XDG_DATA_HOME/nvim/{backup,swap,undo,view} && echo "Bootstrapped vim dirs"
+mkdir -pv "${HISTFILE##*/}" "${LESSHISTFILE##*/}" && echo "Bootstrapped history dirs"
+ln -fhsv {$XDG_CONFIG_HOME/,~/.}profile && echo "Bootstrapped profile"
+[[ $(uname) == "Darwin" ]] && ln -fhsv {$XDG_CONFIG_HOME/,~/Library/LaunchAgents/}org.freedesktop.xdg.plist && echo "Bootstrapped launchd"
